832cc159e63b0b70c7e6dce0e5081c1c15b59925
ggyfykfl <153871009+ggyfykfl@users.noreply.github.com>
2025-04-26 16:44:23 +0300

    Add files via upload

---
+++ 1лр/код/1задание/1задание.cpp
@@ -0,0 +1,15 @@
+﻿#include "Auxil.h"
+#include <iostream>
+
+int main() {
+    setlocale(0, "");
+    auxil::start();
+
+    double randomDouble = auxil::dget(1.0, 10.0);
+    int randomInt = auxil::iget(1, 10);
+
+    std::cout << "Случайное действительное число: " << randomDouble << std::endl;
+    std::cout << "Случайное целое число: " << randomInt <<"\n"<< std::endl;
+
+    return 0;
+}
