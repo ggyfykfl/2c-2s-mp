337e07631ed68e7cbb9eb3888ae610003cc1d7ba
ggyfykfl <153871009+ggyfykfl@users.noreply.github.com>
2025-04-26 16:51:58 +0300

    Add files via upload

---
+++ 2лр/1задание/Main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include "Combi.h"
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	char  AA[][2] = { "A", "B", "C", "D" };
+	std::cout << "Генератор множества всех подмножеств";
+	std::cout << std::endl << "Исходное множество: ";
+	std::cout << "{ ";
+	for (int i = 0; i < sizeof(AA) / 2; i++)
+		std::cout << AA[i] << ((i < sizeof(AA) / 2 - 1) ? ", " : " ");
+	std::cout << "}";
+	std::cout << std::endl << "Генерация всех подмножеств  ";
+	combi::subset s1(sizeof(AA) / 2);
+	int  n = s1.getfirst();
+	while (n >= 0)
+	{
+		std::cout << std::endl << "{ ";
+		for (int i = 0; i < n; i++)
+			std::cout << AA[s1.ntx(i)] << ((i < n - 1) ? ", " : " ");
+		std::cout << "}";
+		n = s1.getnext();\
+	};
+	std::cout << std::endl << "всего: " << s1.count() << std::endl;
+	system("pause");
+	return 0;
+}
