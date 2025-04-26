999d5d40628ef8d1ff9ce1804a56ffa1d9604699
ggyfykfl <153871009+ggyfykfl@users.noreply.github.com>
2025-04-26 16:52:43 +0300

    Add files via upload

---
+++ 2лр/2задание/Combi.h
@@ -0,0 +1,21 @@
+#pragma once 
+namespace combi
+{
+    struct  xcombination
+    {
+        short   n,
+            m,
+            * sset;
+        xcombination(
+            short n = 1,
+            short m = 1
+        );
+        void reset();
+        short getfirst();
+        short getnext();
+        short ntx(short i);
+        unsigned __int64 nc;
+        unsigned __int64 count() const;
+    };
+};
+#pragma once
